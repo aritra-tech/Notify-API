4e9762cecdb33cc883811e604beb1c43902f649f
Aritra Das <aritrarick2002@gmail.com>
2023-07-23 22:33:49 +0530

    Created db file

---
+++ src/main/kotlin/com/aritra/data/NoteDatabase.kt
@@ -0,0 +1,35 @@
+package com.aritra.data
+
+import com.aritra.data.model.Notes
+import org.bson.types.ObjectId
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.eq
+import org.litote.kmongo.reactivestreams.KMongo
+
+
+private val client = KMongo.createClient().coroutine
+private val db = client.getDatabase("NotesDatabase")
+
+private val note = db.getCollection<Notes>()
+
+
+suspend fun getNotesForId(id: String) : Notes? {
+    return note.findOneById(id)
+}
+
+suspend fun addOrUpdateNote(notes: Notes): Boolean {
+    val noteExists = note.findOneById(notes.id) != null
+    return if (noteExists) {
+        note.updateOneById(notes.id, notes).wasAcknowledged()
+    } else {
+        notes.id = ObjectId().toString()
+        note.insertOne(notes).wasAcknowledged()
+    }
+}
+
+suspend fun deleteNote(noteId: String) : Boolean {
+    val notesList = note.findOneById(Notes::id eq noteId)
+    notesList?.let {
+        return note.deleteOneById(notesList.id).wasAcknowledged()
+    } ?: return false
+}
