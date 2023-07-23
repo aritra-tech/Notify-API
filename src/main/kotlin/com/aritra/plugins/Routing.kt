b8c2f91ceb052e5c874479c9beae5b34cfa94fed
Aritra Das <aritrarick2002@gmail.com>
2023-07-23 22:56:11 +0530

    Fixed the bug 🪓

--- src/main/kotlin/com/aritra/plugins/Routing.kt
+++ src/main/kotlin/com/aritra/plugins/Routing.kt
@@ -1,11 +1,13 @@
 package com.aritra.plugins
 
+import com.aritra.routes.noteRoutes
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
     routing {
+        noteRoutes()
         get("/") {
             //This is a comment
             call.respondText("Hello World!")
